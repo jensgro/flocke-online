8cdd6c839af29d905785044eda0c127496cbcdf6
Jens Grochtdreis <jens@grochtdreis.de>
2022-02-21 21:51:11 +0100

    Wissenswertes

--- .eleventy.js
+++ .eleventy.js
@@ -66,8 +66,9 @@ module.exports = function(eleventyConfig) {
       else return 0;
     })
   );
+
   eleventyConfig.addCollection("infosDescending", (collection) =>
-    collection.getFilteredByGlob("./wissenswertes/*.md").sort((a, b) => {
+    collection.getFilteredByGlob("./infos/*.md").sort((a, b) => {
       if (a.data.title < b.data.title) return -1;
       else if (a.data.title > b.data.title) return 1;
       else return 0;
